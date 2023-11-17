e4731c8c0d48a9585d550e1864bab967eac2ae5d
Trey Orr <treyorr20@gmail.com>
2023-11-16 22:21:47 -0500

    First commit

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,20 @@
+import { AppShell } from '@mantine/core'
+
+import { NavbarSearch } from './components/NabarSearch'
+
+function App() {
+  return (
+    <AppShell header={{ height: 60 }} navbar={{ width: 300, breakpoint: 'sm' }} padding="lg">
+      <AppShell.Header>
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar>
+        <NavbarSearch />
+      </AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  )
+}
+export default App
---
+++ src/renderer/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+)
