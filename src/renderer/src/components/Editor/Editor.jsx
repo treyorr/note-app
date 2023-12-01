2219d214b22fa3b2a120774288ac7d939b0ce598
Trey Orr <treyorr20@gmail.com>
2023-12-01 14:16:26 -0500

    save edits on leave

--- src/renderer/src/components/Editor/Editor.jsx
+++ src/renderer/src/components/Editor/Editor.jsx
@@ -8,10 +8,12 @@ import Superscript from '@tiptap/extension-superscript'
 import SubScript from '@tiptap/extension-subscript'
 import { useState, useEffect } from 'react'
 import { useFileContext } from '../../FileContext'
-import { Button, Text } from '@mantine/core'
+import { Button, Text, Modal, Group } from '@mantine/core'
 import { IconDeviceFloppy, IconEdit } from '@tabler/icons-react'
+import { useDisclosure } from '@mantine/hooks'
 
 export function NoteEditor() {
+  const [opened, { close, open }] = useDisclosure(false)
   const [editable, setEditable] = useState(false)
   const [noteContentLoaded, setNoteContentLoaded] = useState(false)
   const { currentOpenFile } = useFileContext()
@@ -46,12 +48,12 @@ export function NoteEditor() {
     )
   }
 
-  async function handleSave() {
+  async function handleSave(path) {
     if (editor) {
-      setEditable((prevEditable) => !prevEditable)
+      setEditable(false)
       try {
         let args = {
-          nPath: currentOpenFile,
+          nPath: path,
           content: editor.getHTML()
         }
         const response = await window.electron.ipcRenderer.invoke('save-note-content', args)
@@ -63,26 +65,34 @@ export function NoteEditor() {
       }
     }
   }
+
+  const fetchNoteContent = async () => {
+    try {
+      let args = {
+        nPath: currentOpenFile
+      }
+      const response = await window.electron.ipcRenderer.invoke('get-note-content', args)
+      if (response && response.success) {
+        setFilePath(currentOpenFile)
+        editor.commands.setContent(response.data)
+        setDate(response.date)
+      }
+    } catch (error) {
+      console.error('Error fetching note content:', error)
+    }
+  }
+
   //initial content load of note
   useEffect(() => {
-    const fetchNoteContent = async () => {
-      try {
-        let args = {
-          nPath: currentOpenFile
-        }
-        const response = await window.electron.ipcRenderer.invoke('get-note-content', args)
-        if (response && response.success) {
-          setFilePath(currentOpenFile)
-          editor.commands.setContent(response.data)
-          setDate(response.date)
-        }
-      } catch (error) {
-        console.error('Error fetching note content:', error)
-      }
-    }
-
+    // if switching notes
     if ((!noteContentLoaded && editor) || currentOpenFile != filePath) {
-      fetchNoteContent()
+      // if we have unsaved changes, ask user if they want to save before leaving
+      if (editable == true) {
+        open()
+      } else {
+        fetchNoteContent()
+        setEditable(false)
+      }
     }
   }, [editor, noteContentLoaded, currentOpenFile])
 
@@ -91,99 +101,125 @@ export function NoteEditor() {
     if (!editor) {
       return undefined
     }
-
     editor.setEditable(editable)
   }, [editor, editable])
+
+  //handle unsaved changes
+  function saveChanges() {
+    handleSave(filePath)
+    handleClose()
+  }
+  function handleClose() {
+    fetchNoteContent()
+    setEditable(false)
+    close()
+  }
 
   if (!editor) {
     return null
   }
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        height: '100vh',
-        overflow: 'auto'
-      }}
-    >
-      <RichTextEditor mt="40px" mb="10px" w={'85%'} editor={editor}>
-        <RichTextEditor.Toolbar
-          sticky
-          w={'100%'}
-          style={{ justifyContent: 'center', top: 0, zIndex: 100 }}
-        >
-          {editable ? (
-            <>
-              <RichTextEditor.ControlsGroup>
-                <RichTextEditor.Bold />
-                <RichTextEditor.Italic />
-                <RichTextEditor.Underline />
-                <RichTextEditor.Strikethrough />
-                <RichTextEditor.ClearFormatting />
-                <RichTextEditor.Highlight />
-                <RichTextEditor.Code />
-              </RichTextEditor.ControlsGroup>
+    <>
+      <Modal opened={opened} onClose={handleClose} title="Unsaved Changes">
+        <Text>
+          You have unsaved changes, would you like to save them before leaving? If you choose not
+          to, your changes to the note will be lost.
+        </Text>
+        <Group mt={20} justify="center" gap="xl">
+          <Button size="compact-xl" color="green" onClick={saveChanges}>
+            Save
+          </Button>
+          <Button size="compact-xl" color="red" onClick={handleClose}>
+            Discard
+          </Button>
+        </Group>
+      </Modal>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '100vh',
+          overflow: 'auto'
+        }}
+      >
+        <RichTextEditor mt="40px" mb="10px" w={'85%'} editor={editor}>
+          <RichTextEditor.Toolbar
+            sticky
+            w={'100%'}
+            style={{ justifyContent: 'center', top: 0, zIndex: 100 }}
+          >
+            {editable ? (
+              <>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.Bold />
+                  <RichTextEditor.Italic />
+                  <RichTextEditor.Underline />
+                  <RichTextEditor.Strikethrough />
+                  <RichTextEditor.ClearFormatting />
+                  <RichTextEditor.Highlight />
+                  <RichTextEditor.Code />
+                </RichTextEditor.ControlsGroup>
 
-              <RichTextEditor.ControlsGroup>
-                <RichTextEditor.H1 />
-                <RichTextEditor.H2 />
-                <RichTextEditor.H3 />
-                <RichTextEditor.H4 />
-              </RichTextEditor.ControlsGroup>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.H1 />
+                  <RichTextEditor.H2 />
+                  <RichTextEditor.H3 />
+                  <RichTextEditor.H4 />
+                </RichTextEditor.ControlsGroup>
 
-              <RichTextEditor.ControlsGroup>
-                <RichTextEditor.Blockquote />
-                <RichTextEditor.Hr />
-                <RichTextEditor.BulletList />
-                <RichTextEditor.OrderedList />
-                <RichTextEditor.Subscript />
-                <RichTextEditor.Superscript />
-              </RichTextEditor.ControlsGroup>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.Blockquote />
+                  <RichTextEditor.Hr />
+                  <RichTextEditor.BulletList />
+                  <RichTextEditor.OrderedList />
+                  <RichTextEditor.Subscript />
+                  <RichTextEditor.Superscript />
+                </RichTextEditor.ControlsGroup>
 
-              <RichTextEditor.ControlsGroup>
-                <RichTextEditor.Link />
-                <RichTextEditor.Unlink />
-              </RichTextEditor.ControlsGroup>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.Link />
+                  <RichTextEditor.Unlink />
+                </RichTextEditor.ControlsGroup>
 
-              <RichTextEditor.ControlsGroup>
-                <RichTextEditor.AlignLeft />
-                <RichTextEditor.AlignCenter />
-                <RichTextEditor.AlignJustify />
-                <RichTextEditor.AlignRight />
-              </RichTextEditor.ControlsGroup>
-              <Button
-                size="compact-sm"
-                color="green"
-                style={{ marginLeft: 'auto' }}
-                leftSection={<IconDeviceFloppy size={16} />}
-                onClick={() => handleSave()}
-              >
-                Save
-              </Button>
-            </>
-          ) : (
-            <>
-              <Text>{getPathText()}</Text>
-              <Text c="gray.6" style={{ marginLeft: 'auto' }}>
-                {date}
-              </Text>
-              <Button
-                size="compact-sm"
-                variant="outline"
-                color="gray"
-                leftSection={<IconEdit size={16} />}
-                onClick={() => setEditable(true)}
-              >
-                Edit
-              </Button>
-            </>
-          )}
-        </RichTextEditor.Toolbar>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.AlignLeft />
+                  <RichTextEditor.AlignCenter />
+                  <RichTextEditor.AlignJustify />
+                  <RichTextEditor.AlignRight />
+                </RichTextEditor.ControlsGroup>
+                <Button
+                  size="compact-sm"
+                  color="green"
+                  style={{ marginLeft: 'auto' }}
+                  leftSection={<IconDeviceFloppy size={16} />}
+                  onClick={() => handleSave(currentOpenFile)}
+                >
+                  Save
+                </Button>
+              </>
+            ) : (
+              <>
+                <Text>{getPathText()}</Text>
+                <Text c="gray.6" style={{ marginLeft: 'auto' }}>
+                  {date}
+                </Text>
+                <Button
+                  size="compact-sm"
+                  variant="outline"
+                  color="gray"
+                  leftSection={<IconEdit size={16} />}
+                  onClick={() => setEditable(true)}
+                >
+                  Edit
+                </Button>
+              </>
+            )}
+          </RichTextEditor.Toolbar>
 
-        <RichTextEditor.Content />
-      </RichTextEditor>
-    </div>
+          <RichTextEditor.Content />
+        </RichTextEditor>
+      </div>
+    </>
   )
 }
